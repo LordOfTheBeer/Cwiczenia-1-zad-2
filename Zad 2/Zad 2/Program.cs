b5ca25063e1c49c44de93c2c31ac670f8eee1413
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-11-06 20:46:57 +0100

    Add files via upload

---
+++ Zad 2/Zad 2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double liczba1, liczba2;
+            
+            double wynik;
+
+            Console.WriteLine("Podaj liczbe");
+
+            liczba1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj liczbe");
+
+            liczba2 = double.Parse(Console.ReadLine());
+
+            wynik = (liczba1 + liczba2) / 2;
+
+            Console.WriteLine(wynik);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
